4c58c763850eed4120d8f3d4ed0e2afa5417cd7c
greengoblinalex <greengoblinalex@mail.ru>
2023-12-20 00:40:59 +0300

    Update

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -14,7 +14,8 @@ SPIDER_MODULES = ['pep_parse.spiders']
 NEWSPIDER_MODULE = 'pep_parse.spiders'
 
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
+# Crawl responsibly by identifying yourself
+# (and your website) on the user-agent
 # USER_AGENT = 'pep_parse (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
@@ -39,8 +40,9 @@ ROBOTSTXT_OBEY = True
 
 # Override the default request headers:
 # DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
+#     'Accept': ('text/html,application/'
+#                'xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+#     'Accept-Language': 'en',
 # }
 
 # Enable or disable spider middlewares
@@ -81,7 +83,8 @@ ITEM_PIPELINES = {
 # AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# See https://docs.scrapy.org/en/latest/topics
+# /downloader-middleware.html#httpcache-middleware-settings
 # HTTPCACHE_ENABLED = True
 # HTTPCACHE_EXPIRATION_SECS = 0
 # HTTPCACHE_DIR = 'httpcache'
